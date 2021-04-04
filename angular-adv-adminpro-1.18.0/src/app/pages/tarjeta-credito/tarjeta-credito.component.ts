b718ee7ca19172626ea55f797be70e5333d594d9
Cristian Ramirez <cristian.ramirez.ballestero@gmail.com>
2021-04-04 22:41:57 +0200

    Form tarjeta mejorado

--- angular-adv-adminpro-1.18.0/src/app/pages/tarjeta-credito/tarjeta-credito.component.ts
+++ angular-adv-adminpro-1.18.0/src/app/pages/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 const VISA: string = "visa";
@@ -10,9 +10,14 @@ const MASTERCARD: string = "mastercard";
   styleUrls: ['../../../assets/css/pages/tarjeta-credito.css', './tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
+  @ViewChild('spanMes') spanMes: ElementRef;
+  @ViewChild('spanYear') spanYear: ElementRef;
+  @ViewChild('pNumero') pNumero: ElementRef;
+  @ViewChild('pTitular') pTitular: ElementRef;
 
   public tipos = [VISA, MASTERCARD];
-
+  public meses = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  public years: number[] = [];
 
   public esVisa: boolean = true;
 
@@ -21,14 +26,23 @@ export class TarjetaCreditoComponent implements OnInit {
       tipo: ['', Validators.required],
       titular: ['', Validators.required],
       numero: ['', Validators.required],
-      fechaCaducidad: ['', Validators.required],
+      mes: ['', Validators.required],
+      year: ['', Validators.required],
       cvv: ['', Validators.required],
     }
   );
+
+  //Tarda 5 años en caducar la tarjeta
+  private tiempoCaducidadTarjetaEnYears: number = 5;
+
   constructor(public fb: FormBuilder) { }
 
   ngOnInit(): void {
+    const añoActual = new Date().getFullYear();
 
+    for (let i = 0; this.tiempoCaducidadTarjetaEnYears > i; i++) {
+      this.years[i] = añoActual + i;
+    }
   }
   guardarTarjetaCredito() {
     console.log("Guardando tarjeta");
@@ -41,5 +55,23 @@ export class TarjetaCreditoComponent implements OnInit {
     } else if (tipo === MASTERCARD) {
       this.esVisa = false;
     }
+  }
+  onChangeMes() {
+    const { mes } = this.tarjetaCreditoForm.value;
+
+    this.spanMes.nativeElement.innerHTML = mes;
+  }
+  onChangeYear() {
+    const { year } = this.tarjetaCreditoForm.value;
+
+    this.spanYear.nativeElement.innerHTML = year;
+  }
+  onChangeNumero() {
+    const { numero } = this.tarjetaCreditoForm.value;
+    this.pNumero.nativeElement.innerHTML = numero;
+  }
+  onChangeTitular() {
+    const { titular } = this.tarjetaCreditoForm.value;
+    this.pTitular.nativeElement.innerHTML = titular;
   }
 }
