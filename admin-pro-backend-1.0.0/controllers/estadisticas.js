2138cb6c0d34ce431d7f08cbd84b781d273433c6
Cristian Ramirez <cristian.ramirez.ballestero@gmail.com>
2021-04-28 23:31:24 +0200

    Backend haciendo estadisticas

--- admin-pro-backend-1.0.0/controllers/estadisticas.js
+++ admin-pro-backend-1.0.0/controllers/estadisticas.js
@@ -10,21 +10,34 @@ const getEstadisticasProductos = async (req, res) => {
             .populate('producto', 'nombre precio')
 
         const arrayLineaPedidos = [];
-        for (var i in lineaPedidos) {
+     /*   for (var i in lineaPedidos) {
             arrayLineaPedidos.push([i, lineaPedidos[i]]);
-        }
+        }*/
+        const aaa=JSON.parse(lineaPedidos.toString());
+        console.log(aaa);
+        console.log(aaa.cantidad);
 
+        let estadisticasProductos = new Map();
 
         for (let i = 0; lineaPedidos.length > i; i++) {
-            const cantidad = arrayLineaPedidos[i].cantidad;
-            const precio = arrayLineaPedidos[i].producto.nombre;
-            
+            if (arrayLineaPedidos[i].cantidad != null && arrayLineaPedidos[i].producto.nombre != null) {
+                const cantidad = arrayLineaPedidos[i].cantidad;
+                const nombreProducto = arrayLineaPedidos[i].producto.nombre;
+                console.log("Cantidad:"+cantidad);
+                console.log(nombreProducto);
+
+                if (estadisticasProductos.size == 0) {
+                    estadisticasProductos.set(nombreProducto, cantidad);
+                }
+            }
         }
 
+        const estadisticas = mapToObj(estadisticasProductos);
+        const bobo = { "x": 2 };
 
         res.json({
             ok: true,
-            lineaPedidos,
+            bobo,
         });
 
     } catch (error) {
@@ -35,6 +48,17 @@ const getEstadisticasProductos = async (req, res) => {
         })
     }
 }
+
+function mapToObj(inputMap) {
+    let obj = {};
+
+    inputMap.forEach(function (value, key) {
+        obj[key] = value
+    });
+
+    return  JSON.stringify(obj);
+}
+
 
 module.exports = {
     getEstadisticasProductos
