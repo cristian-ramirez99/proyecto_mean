23e109fa4717376deafd10d3279449168ce3cd97
Cristian Ramirez <cristian.ramirez.ballestero@gmail.com>
2021-05-14 12:31:12 +0200

    Registrar checked terminos quitado

--- angular-adv-adminpro-1.18.0/src/app/auth/register/register.component.ts
+++ angular-adv-adminpro-1.18.0/src/app/auth/register/register.component.ts
@@ -8,54 +8,54 @@ import { UsuarioService } from '../../services/usuario.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: [ './register.component.css' ]
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
 
   public formSubmitted = false;
 
   public registerForm = this.fb.group({
-    nombre: ['', Validators.required ],
-    email: ['', [ Validators.required, Validators.email ] ],
-    password: ['', Validators.required ],
-    password2: ['', Validators.required ],
-    terminos: [ true, Validators.required ],
+    nombre: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
+    terminos: [false, Validators.required],
   }, {
     validators: this.passwordsIguales('password', 'password2')
   });
 
-  constructor( private fb: FormBuilder,
-               private usuarioService: UsuarioService,
-               private router: Router ) { }
+  constructor(private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router) { }
 
   /*Si todos los campos son validos se hace peticion http para crear usuario*/
   crearUsuario() {
     this.formSubmitted = true;
 
     //Si algun campo invalido se acaba la ejecución del método
-    if ( this.registerForm.invalid ) {
+    if (this.registerForm.invalid || this.registerForm.value.terminos == false) {
       return;
     }
 
     // Realizar el posteo
-    this.usuarioService.crearUsuario( this.registerForm.value )
-        .subscribe( resp => {
-          
-          // Navegar al Dashboard
-          this.router.navigateByUrl('/');
+    this.usuarioService.crearUsuario(this.registerForm.value)
+      .subscribe(resp => {
 
-        }, (err) => {
-          // Si sucede un error
-          Swal.fire('Error', err.error.msg, 'error' );
-        });
+        // Navegar al Dashboard
+        this.router.navigateByUrl('/');
+
+      }, (err) => {
+        // Si sucede un error
+        Swal.fire('Error', err.error.msg, 'error');
+      });
 
 
   }
 
   //Comprueba si los campos son valido
-  campoNoValido( campo: string ): boolean {
-    
-    if ( this.registerForm.get(campo).invalid && this.formSubmitted ) {
+  campoNoValido(campo: string): boolean {
+
+    if (this.registerForm.get(campo).invalid && this.formSubmitted) {
       return true;
     } else {
       return false;
@@ -68,28 +68,28 @@ export class RegisterComponent {
     const pass1 = this.registerForm.get('password').value;
     const pass2 = this.registerForm.get('password2').value;
 
-    if ( (pass1 !== pass2) && this.formSubmitted ) {
+    if ((pass1 !== pass2) && this.formSubmitted) {
       return true;
     } else {
       return false;
     }
 
-  } 
+  }
 
   //Comprueba que se hayan aceptado los terminos
-  aceptaTerminos() {
+  aceptarTerminos() {
     return !this.registerForm.get('terminos').value && this.formSubmitted;
   }
 
-  
-  passwordsIguales(pass1Name: string, pass2Name: string ) {
 
-    return ( formGroup: FormGroup ) => {
+  passwordsIguales(pass1Name: string, pass2Name: string) {
+
+    return (formGroup: FormGroup) => {
 
       const pass1Control = formGroup.get(pass1Name);
       const pass2Control = formGroup.get(pass2Name);
 
-      if ( pass1Control.value === pass2Control.value ) {
+      if (pass1Control.value === pass2Control.value) {
         pass2Control.setErrors(null)
       } else {
         pass2Control.setErrors({ noEsIgual: true })
